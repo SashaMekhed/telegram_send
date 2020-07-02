0e983c6b9f880a9f044d05ec1f8afd4a2cce5194
SashaMekhed <66962102+SashaMekhed@users.noreply.github.com>
2020-07-02 16:54:26 +0300

    Add files via upload

--- telegram.php
+++ telegram.php
@@ -1,26 +1,18 @@
- <?php
+<?php
 
-$name = $_POST ['user-name'];
-$phone = $_POST ['user-phone'];
+$name = $_POST['name'];
+$phone = $_POST['phone'];
 $email = $_POST ['user-email'];
 $token = "1358494187:AAFoBe-NGTDWqtvEWyc-qRfIIwSCRYAl5Ok";
 $chat_id = "-465074769";
 $arr = array(
-	'Имя пользователя: ' => $name,
-	'Телефон ' => $phone,
-	'Email: ' => $email
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+  'Почта: ' => $email
 );
-
 foreach($arr as $key => $value) {
-	$txt .= "<b>".$key."</b> ".$value."%0A";
+  $txt .= "<b>".$key."</b> ".$value."%0A";
 };
 
-$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}", "r");
-
-if ($sendToTelegram) {
-	header('Location: thank-you.html');
-}else {
-	echo "Error";
-}
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 ?>
-
